73ef5e3ea2f64cf3de226919772cc9abb3e10574
qingmulanyun <wenbo199019@gmail.com>
2018-05-30 11:34:41 +1000

    add page limitation to batch transaction model

--- lib/promisepay/models/batch_transaction.rb
+++ lib/promisepay/models/batch_transaction.rb
@@ -3,7 +3,7 @@ module Promisepay
   class BatchTransaction < BaseModel
     # @return [Array<Promisepay::Item>]
     def items
-      response = JSON.parse(@client.get("batch_transactions/#{send(:id)}/items").body)
+      response = JSON.parse(@client.get("batch_transactions/#{send(:id)}/items?limit=100").body)
       items = response.key?('items') ? response['items'] : []
       items.map { |attributes| Promisepay::Item.new(@client, attributes) }
     end
